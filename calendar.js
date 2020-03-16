49fbee7d42e2d65f69cad9229413370d323d6963
Helen Jiang <helen.jiang@wustl.edu>
2020-03-16 14:10:28 -0500

    ahhh

--- calendar.js
+++ calendar.js
@@ -1,11 +1,12 @@
 //variables
-let today=new Date();
+let today= new Date();
 let currentMonth = new Month(2020, 3); 
 let startmonth=today.getMonth();
 let startyear=today.getFullYear();
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; 
-let calendarTable = document.getElementById("calendar");
+let calendar = document.getElementById("calendar");
+
 
 document.addEventListener('DOMContentLoaded', function (event) {
     console.log("inside DOMContentLoaded");
@@ -14,25 +15,76 @@ document.addEventListener('DOMContentLoaded', function (event) {
   
   }, false);
 
-  //tobe finished
-function showcalendar(month,year){
+  function updateCalendar() {
+	document.getElementById('Month').innerHTML = months[currentMonth.month];
+	document.getElementById('Year').innerHTML = currentMonth.year;
+	var weeks = currentMonth.getWeeks();
+	let calendar = document.getElementById('calendar');
+	calendar.innerHTML = "";
+	// create a new row for each week
+	for (var w in weeks) {
+	  let thisWeek = document.createElement('tr');
+	  var days = weeks[w].getDates();
+	  // days contains normal JavaScript Date objects by creating a new cell for each day
+	  for (var d in days) {
+		var today = document.createElement('td');
+		// making the cells which have dates not belonging to the month blank
+		if (days[d].getMonth() != currentMonth.month) {
+		  today.appendChild(document.createTextNode(" "));
+		}
+		else {
+		  // populating each cell with info from days array
+		  today.setAttribute("id", "day" + days[d].getDate())
+		  today.appendChild(document.createTextNode(days[d].getDate()));
+		}
+		thisWeek.appendChild(today);
+		// You can see console.log() output in your JavaScript debugging tool, like Firebug,
+		// WebWit Inspector, or Dragonfly.
+	  }
+	  calendar.appendChild(thisWeek);
+	}
+  }
+  
 
-
-
-}
-
-function updateCalendar(){
-
-
-}
 
 function nextMonth(today){
+ //currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+ currentMonth = currentMonth.nextMonth();
+  document.getElementById('Month').innerHTML = months[currentMonth.month];
+  document.getElementById('Year').innerHTML = currentMonth.year;
+ //updateCalendar(currentMonth, currentYear);
+}
+
+function prevMonth(today){
+     currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+     currentMonth = currentMonth.prevMonth();
+     updateCalendar(currentMonth, currentYear);
+}
+
+$(function () {
+ $("#nextbutton").on("click", () => {
+   nextMonth();
+   updateCalendar();
+  
+ })
+ $("#previousbutton").on("click", () => {
+   prevMonth();
+   updateCalendar();
+
+ })
+  })
+
+
+  
+function jump(today){
 
 
 }
-function prevMonth(today){
 
 
+
+
+function updateEvents(){
 
 }
 
