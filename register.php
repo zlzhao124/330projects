687f09b655011e1b5175fbbbc32bb14f75f38cf6
zlzhao124 <zlzhao@wustl.edu>
2020-03-16 21:47:24 -0500

    ok

---
+++ register.php
@@ -0,0 +1,47 @@
+<?php
+
+
+require 'database.php';
+header("Content-Type: application/json");
+
+$userName = (String)$_POST['username'];
+$password = (String)$_POST['pass'];
+
+$addPassword = password_hash($password, PASSWORD_DEFAULT);
+$stmt = $mysqli->prepare("insert into users (username, password) values (?, ?)");
+if(!$stmt){
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+    exit;
+}
+  // Check the username that was entered to make sure that it is not a duplicate, and that both the username and password are nonempty string
+
+if (strlen($userName) > 0 && strlen($password)> 0){
+
+    $stmt->bind_param('ss', $userName, $addPassword);
+
+    if (!$stmt->execute()){
+
+    echo json_encode(array(
+      "success" => false,
+      "message" => "Username already exists"
+    ));
+
+}
+else{
+   echo json_encode(array("success" => true));
+}
+
+    $stmt->close();
+    exit;
+}
+
+else{
+ echo json_encode(array(
+      "success" => false,
+      "message" => "Username/password invalid"
+    ));
+$stmt->close();
+    exit;
+}
+?>
+
